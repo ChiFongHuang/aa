fdc0a4c2de5d3de0e0b9ba152a82dfa47c8170b5
chifonghuang <chifong.huang@mail.utoronto.ca>
2024-06-10 01:19:00 -0400

    first

--- Demo.java
+++ Demo.java
@@ -4,9 +4,11 @@ public class Demo {
     public static void main(String[] args) {
         String a = new String("Hello");
         String b = "Hello";
+        System.out.println(a == b);
         // first
         // second
         // good
         // did not add
+        // second try
     }
 }
