dd5eb3ad2419430647c10dbae039db2794ecdd21
ChiFongHuang <chifong.huang@mail.utoronto.ca>
2024-06-10 00:50:39 -0400

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Demo {
+    public static void main(String[] args) {
+        String a = new String("Hello");
+        String b = "Hello";
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+    }
+}
